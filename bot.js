a2d30278196aa31578ff912395c1dea125ac1a61
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-05-28 20:41:26 +0300

    test

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('guildMemberAdd', (member) => {
     .setColor('00ff00')
     .setFooter('Rainbow`s server 🌈 Welcome!');
     
-    channel.send({embed});
+    member.send({embed});
     
 })
 
