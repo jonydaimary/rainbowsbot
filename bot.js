b3f8c61aa2a646700c5ed38fb6b689a489958f66
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-06-11 08:50:27 +0300

    test

--- bot.js
+++ bot.js
@@ -63,11 +63,6 @@ client.on("message", async message => {
     if(message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
-if(command === `typing`) {
-    message.channel.startTyping()
-
-}
     
     if (command === 'vip') {
         let embed = new Discord.RichEmbed()
