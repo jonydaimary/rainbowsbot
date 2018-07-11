9a67a702b992a966a5724b3e24e5ef4670c5be0e
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-11 13:31:08 +0300

    test

--- bot.js
+++ bot.js
@@ -67,6 +67,7 @@ client.on("message", async message => {
     const command = args.shift().toLowerCase();
 
     if (command === 'a') {
+        message.delete();
         let embed = new Discord.RichEmbed()
         .addField(`ДОБРО ПОЖАЛОВАТЬ`, `Перед тем, как начать общение на нашем севере, ознакомьтесь с основной информацией`)
         .setFooter(`Администрация | 11.07.2018`)
@@ -77,6 +78,7 @@ client.on("message", async message => {
         }
 
     if (command === 'b') {
+        message.delete();
         let embed = new Discord.RichEmbed()
         .addField(`ПОСТОЯННАЯ ССЫЛКА НА СЕРВЕР`, `https://discord.gg/qZWdVRj`)
         .addField(`• donationallerts`, `http://www.donationalerts.ru/r/rainbows_server`)
@@ -88,6 +90,7 @@ client.on("message", async message => {
         }
 
     if (command === 'c') {
+        message.delete();
         let embed = new Discord.RichEmbed()
         .addField(`О НОВИЧКАХ`,'У каждого есть роль **Новичок**. Она выдаётся при подключении к серверу. Новичок не имеет никаких особых прав и привилегий на сервере')
         .setColor(`#800080`)
