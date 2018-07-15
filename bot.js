4b5e9e5c1452670fc3d6493ab287a1f13a0cfbb2
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-15 19:37:46 +0300

    test

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const token = process.env.TOKEN
-const ms = require("ms");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
