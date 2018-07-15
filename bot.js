2114d9a79cfb0cd41d92ee300f3aa286c9c8ff7b
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-15 20:18:48 +0300

    test

--- bot.js
+++ bot.js
@@ -69,6 +69,8 @@ module.exports.run = async (bot, message, args) => {
 
   //!tempmute @user 1s/m/h/d
 
+if(command === 'tempmute') {
+  
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Couldn't find user.");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
@@ -109,6 +111,7 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
   name: "tempmute"
+}
 }   
  
  if (command === 'a') {
