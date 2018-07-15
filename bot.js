c6ae46ede7fcdaae425961924b8460219aaf1223
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-15 20:12:44 +0300

    test

--- bot.js
+++ bot.js
@@ -95,7 +95,6 @@ module.exports.run = async (bot, message, args) => {
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
 
-  
   await(tomute.addRole(muterole.id));
   message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
 
