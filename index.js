358e741bb4eb4a88467a0c6060ee14e22accb88f
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-19 20:17:50 +0300

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+const Discord = require("discord.js");
+const fs = require("fs");
+let xp = require("./xp.json");
+
+let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Уровень повышен!")
+    .setColor(purple)
+    .addField("Новый уровень", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+  });
+  
+bot.login('NDM0Nzc3NDk1NTIzNDI2MzI1.DctjiQ.sMIAXfE34u7gC2CYfE0MATsaSec');
---
+++ level.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+let xp = require("../xp.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
+}
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
+
+  let lvlEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#800080")
+  .addField("Уровень", curlvl, true)
+  .addField("XP", curxp, true)
+  .setFooter(`${difference} XP до следующего уровня`, message.author.displayAvatarURL);
+
+  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
+
+}
+
+module.exports.help = {
+  name: "level"
+}
