f1e4050c64eca864eec16e001ead012bd79b27be
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-19 19:37:09 +0300

    test

--- bot.js
+++ bot.js
@@ -65,6 +65,37 @@ client.on("message", async message => {
  // Direct Messages - #00ff00
  // Chat messages - #800080
 
+ let purple = botconfig.purple;
+ let xp = require("../xp.json");
+ 
+ module.exports.run = async (bot, message, args) => {
+ 
+   if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+   };
+ }
+   let curxp = xp[message.author.id].xp;
+   let curlvl = xp[message.author.id].level;
+   let nxtLvlXp = curlvl * 300;
+   let difference = nxtLvlXp - curxp;
+ 
+   let lvlEmbed = new Discord.RichEmbed()
+   .setAuthor(message.author.username)
+   .setColor(purple)
+   .addField("Level", curlvl, true)
+   .addField("XP", curxp, true)
+   .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
+ 
+   message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
+ 
+ }
+ 
+ module.exports.help = {
+   name: "level"
+ }
+
  if (command === 'report') {
     let rUser = message.guild.member(message.mentions.users.first() || message.get.members.get(args[0]));
     if(!rUser) return message.channel.send("Не могу найти пользователя");
