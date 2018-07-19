599a5f179f1b82b376889bbd2f3ae930c0f4dc53
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-19 21:14:23 +0300

    test

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 const token = process.env.TOKEN
+const ms = require("ms");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
@@ -65,6 +66,50 @@ client.on("message", async message => {
  // Direct Messages - #00ff00
  // Chat messages - #800080
 
+ module.exports.run = async (bot, message, args) => {
+
+
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tomute) return message.reply("Couldn't find user.");
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+    let muterole = message.guild.roles.find(`name`, "muted");
+  
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "muted",
+          color: "#800080",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+    //end of create role
+    let mutetime = args[1];
+    if(!mutetime) return message.reply("Вы не указали время");
+  
+    await(tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> был замучен на ${ms(ms(mutetime))}`);
+  
+    setTimeout(function(){
+      tomute.removeRole(muterole.id);
+      message.channel.send(`<@${tomute.id}> был размучен!`);
+    }, ms(mutetime));
+  
+  
+  //end of module
+  }
+  
+  module.exports.help = {
+    name: "tempmute"
+  }
  
  if (command === 'report') {
     let rUser = message.guild.member(message.mentions.users.first() || message.get.members.get(args[0]));
