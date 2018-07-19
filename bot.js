0ab1d8e2948d3b4fd322764693a0c481a594342f
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-07-19 20:59:31 +0300

    test

--- bot.js
+++ bot.js
@@ -65,6 +65,24 @@ client.on("message", async message => {
  // Direct Messages - #00ff00
  // Chat messages - #800080
 
+ db.updateValue(message.author.id + message.guild.id, 1).then(i => { // You pass it the key, which is authorID + guildID, then pass it an increase which is 1 in this instance.
+    // It also returns the new updated object, which is what we will use.
+
+    let messages; // Create an empty variable - These IF statements will run if the new amount of messages sent is the same as the number.
+    if (i.value == 25) messages = 25; // Level 1
+    else if (i.value == 50) messages = 50; // Level 2
+    else if (i.value == 100) messages = 100; // Level 3 - You can set these to any number, and any amount of them.
+
+    if (!isNaN(messages)) { // If messages IS STILL empty, run this.
+        db.updateValue(`userLevel_${message.author.id + message.guild.id}`, 1).then(o => { // This returns the updated object of userLevel_ID. 
+            message.channel.send(`You sent ${messages} messages, so you leveled up! You are now level ${o.value}`) // Send their updated level to the channel.
+        })
+    }
+
+})
+
+
+ 
  if (command === 'report') {
     let rUser = message.guild.member(message.mentions.users.first() || message.get.members.get(args[0]));
     if(!rUser) return message.channel.send("Не могу найти пользователя");
