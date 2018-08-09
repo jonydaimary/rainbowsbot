c90306ab733eca7a64a2f7a254071fa543eefdfc
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-08-09 20:05:51 +0300

    test

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ const bot = new commando.Client();
 bot.registry.registerGroup('random', 'Random');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
-
-
-bot.login('NDM0Nzc3NDk1NTIzNDI2MzI1.DctjiQ.sMIAXfE34u7gC2CYfE0MATsaSec');
