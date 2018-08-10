d070c22e8a5e9544f2781e52005c74772b4c3f2e
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-08-10 21:47:22 +0300

    test

--- bot.js
+++ bot.js
@@ -75,7 +75,55 @@ client.on("message", async message => {
     });
 }, 3e5)
 
-  if (command === 'report') {
+//!
+module.exports.run = async (bot, message, args) => {
+
+    //!tempmute @user 1s/m/h/d
+  
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tomute) return message.reply("Couldn't find user.");
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+    let muterole = message.guild.roles.find(`name`, "muted");
+    //start of create role
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "muted",
+          color: "#000000",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      }catch(e){
+        console.log(e.stack);
+      }
+    }
+    //end of create role
+    let mutetime = args[1];
+    if(!mutetime) return message.reply("You didn't specify a time!");
+  
+    await(tomute.addRole(muterole.id));
+    message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  
+    setTimeout(function(){
+      tomute.removeRole(muterole.id);
+      message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(mutetime));
+  
+  
+  //end of module
+  }
+  
+  module.exports.help = {
+    name: "tempmute"
+  }
+//!
+
+if (command === 'report') {
     let rUser = message.guild.member(message.mentions.users.first() || message.get.members.get(args[0]));
     if(!rUser) return message.channel.send("Не могу найти пользователя");
     let reason = args.join(" ").slice(22);
@@ -304,7 +352,7 @@ client.on("message", async message => {
  
         const fetched = await message.channel.fetchMessages({count: deleteCount});
         message.channel.bulkDelete(fetched)
-            .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+            .catch(error => message.reply(`Вы не можете удалить сообщения из-за ${error}`));
     }
     
 });
