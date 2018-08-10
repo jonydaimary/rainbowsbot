061e096cf321e2da2f5939a1a63ff75280aded35
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-08-10 21:10:00 +0300

    test

--- bot.js
+++ bot.js
@@ -66,51 +66,15 @@ client.on("message", async message => {
  // Direct Messages - #00ff00
  // Chat messages - #800080
 
- module.exports.run = async (bot, message, args) => {
+ setInterval(() => {
+    let guild = client.guilds.get('469596213491138570 ');
+    guild.members.forEach((id, member) => {
+        if (member.roles.has('469612245744091181') && !member.roles.has('470625504035078154')) {
+            member.addRole('470625504035078154')
+        }
+    });
+}, 6e5)
 
-
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!tomute) return message.reply("Couldn't find user.");
-    if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
-    let muterole = message.guild.roles.find(`name`, "muted");
-  
-    if(!muterole){
-      try{
-        muterole = await message.guild.createRole({
-          name: "muted",
-          color: "#800080",
-          permissions:[]
-        })
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        });
-      }catch(e){
-        console.log(e.stack);
-      }
-    }
-    //end of create role
-    let mutetime = args[1];
-    if(!mutetime) return message.reply("Вы не указали время");
-  
-    await(tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> был замучен на ${ms(ms(mutetime))}`);
-  
-    setTimeout(function(){
-      tomute.removeRole(muterole.id);
-      message.channel.send(`<@${tomute.id}> был размучен!`);
-    }, ms(mutetime));
-  
-  
-  //end of module
-  }
-  
-  module.exports.help = {
-    name: "tempmute"
-  }
- 
   if (command === 'report') {
     let rUser = message.guild.member(message.mentions.users.first() || message.get.members.get(args[0]));
     if(!rUser) return message.channel.send("Не могу найти пользователя");
