1ffe99794b2eed9b8df2f65e1484a36a136ae4a0
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-08-11 21:26:36 +0300

    test

--- bot.js
+++ bot.js
@@ -87,7 +87,26 @@ if (command === 'mute') {
     .addField(`Кого замутили:`, `${message.mentions.members.first}`)
     .setTimestamp(message.createdAt);
 
+    let mutechannel = message.guild.channels.find(`name`, "стафф-чат");
+
+    message.mutechannel.send(embed)
     message.channel.send(embed)
+
+}
+
+if (command === 'unmute') {
+    let member = message.mentions.members.first();
+    
+    member.removeRole('477550132355661834')
+    
+    let embed = new Discord.RichEmbed()
+    .setDescription("Пользователь размучен!")
+    .setColor("#800080")
+    .addField(`Кого размутили:`, `${member}`)
+    .setTimestamp(message.createdAt);
+    
+    message.channel.send(embed)
+    
     
 
 }
