e2e1dfb97c1cf0f11932304696896c761d2ad684
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-10-24 17:56:24 +0300

    test

--- bot.js
+++ bot.js
@@ -77,6 +77,8 @@ let mRND = 0; //RANDOM  |   mRND = mRND + 1; mALL = mALL + 1;
 let mRNB = 0; //RAINBOW |   mRNB = mRNB + 1; mALL = mALL + 1;
 let mTOL = 0; //TOOLS   |   mTOL = mTOL + 1; mALL = mALL + 1;
 
+let xp = require("xp.json");
+
 let lvl1 = ["text", "text", "text", "text"];
 
 let lvl = (lvl1[Math.floor(Math.random() * lvl1.length)])
