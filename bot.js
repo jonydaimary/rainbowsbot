e8fe837617472773754d7c017dedfa02365cd7b6
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-10-27 16:41:31 +0300

    test

--- bot.js
+++ bot.js
@@ -58,19 +58,11 @@ client.on('guildMemberRemove', (member) => {
     
 })
 
-const colors = ["FF0D00","FF2800","FF3D00","FF4F00","FF5F00","FF6C00","FF7800","FF8300","FF8C00","FF9500","FF9E00","FFA500","FFAD00","FFB400","FFBB00","FFC200","FFC900","FFCF00","FFD600","FFDD00","FFE400","FFEB00","FFF200","FFFA00","F7FE00","E5FB00","D5F800","C6F500","B7F200","A8F000","98ED00","87EA00","74E600","5DE100","41DB00","1DD300","00C618","00BB3F","00B358","00AC6B","00A67C","009E8E","028E9B","06799F","0969A2","0C5DA5","0E51A7","1047A9","133CAC","1531AE","1924B1","1F1AB2","2A17B1","3415B0","3C13AF","4512AE","4E10AE","560EAD","600CAC","6A0AAB","7608AA","8506A9","9702A7","AD009F","BC008D","C7007D","D0006E","D8005F","DF004F","E7003E","EF002A","F80012"];
-const stop = [];
-async function color () {
-    forEachTimeout(colors, (color) => {
-        bot.guilds.forEach((guild) => {
-                if (!stop.includes(guild.id)) {
-                let role = guild.roles.find('name', 'Главный Администратор');
-                if (role && role.editable)
-                    role.setColor(color);
-            }  
-        })
-    }, 1500).then(color);
-}
+function randomStatus() {
+    let status = [`Время ${(new Date(new Date().getTime() + 3*60*60*1000)).toISOString().replace(/(.*?)T/, '').replace(/\..+/, '')+' по МСК'}`, `rb!help | RAINBOWs SERVER`,  `rb!help | RA1NBOW#1111`, `rb!help | https://discord.gg/CtRp5GB`];
+    let rstatus = Math.floor(Math.random() * status.length); mRND = mRND + 1; mALL = mALL + 1;
+    bot.user.setActivity(status[rstatus]);
+}; setInterval(randomStatus, 30000)
 
 const matchingRole = '469612245744091181';
 const givenRole = '470625504035078154';
