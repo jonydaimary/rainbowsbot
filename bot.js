342df985d7a0cde4c6947a9b23f45ffc64742f82
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-11-04 22:40:06 +0300

    test

--- bot.js
+++ bot.js
@@ -83,12 +83,15 @@ if (command === `seen`) {
     let user = message.mentions.members.first();
 
     let tag = user.user.tag
+    
+    let createdAt = user.user.createdAt
 
     let embed = new Discord.RichEmbed()    
 
     .setTitle(`Подробная информация о пользователе`)
     .addField(`USER ID`, user.id)
     .addField(`TAG`, tag)
+    .addField(`CREATED AT`, createdAt)
     .setFooter(`Ranbow's Server | User `)
     .setColor(`#000000`);
 
