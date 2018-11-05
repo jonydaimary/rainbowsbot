da30d49ae6aa945b18289bca199ab46fc2f8c80d
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-11-05 12:26:33 +0300

    test

--- bot.js
+++ bot.js
@@ -88,9 +88,10 @@ if (command === `seen`) {
     
     let createdAt = user.user.createdAt
 
+    let joinedAt = member.user.joinedAt
+
     if (!user ) return message.channel.send('Ошибка');
         let arr = {'online': 'Online', 'dnd': 'Do not distrub', 'idle': 'Idle', 'offline': 'Offline'};
-
 
     let embed = new Discord.RichEmbed()    
 
@@ -100,6 +101,7 @@ if (command === `seen`) {
     .addField(`TAG`, tag)
     .addField(`CREATED AT`, createdAt)
     .addField(`STATUS`, arr[user.presence.status])
+    .addField(`JOINED AT`, joinedAt)
     .setThumbnail(member.user.avatarURL)
     .setFooter(`Ranbow's Server | User `)
     .setColor(`#000000`);
