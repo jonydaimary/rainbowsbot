b02ddbbe976e2e23787e263ed5d0a58201b4f452
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-11-05 19:57:15 +0300

    test

--- bot.js
+++ bot.js
@@ -48,6 +48,25 @@ client.on('guildMemberAdd', (member) => {
     
 })
 
+client.on('message', message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (message.content === '/join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+          .then(connection => { // Connection is an instance of VoiceConnection
+            message.reply('I have successfully connected to the channel!');
+          })
+          .catch(console.log);
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+    }
+  });
+
 client.on('guildMemberRemove', (member) => {
     let embed = new Discord.RichEmbed()
     .setTitle(`Участник покинул сервер`)
