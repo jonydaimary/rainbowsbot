a430fbfd9982ebc438754a335e222af7ef058635
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-11-05 19:58:42 +0300

    test

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ client.on('message', message => {
     // we ignore it
     if (!message.guild) return;
   
-    if (message.content === '/join') {
+    if (command === 'join') {
       // Only try to join the sender's voice channel if they are in one themselves
       if (message.member.voiceChannel) {
         message.member.voiceChannel.join()
