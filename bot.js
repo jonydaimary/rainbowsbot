2781fec42d5cf44f82c3b4e4fc795ea455afbcd2
Ra1nbow1 <38384967+Ra1nbow1@users.noreply.github.com>
2018-11-10 20:53:32 +0300

    test

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on("message", async message => {
             message.channel.send(embed);
             client.channels.get('469600390455885833').send(embed);
             client.off('messageReactionAdd', reactionListener);
-        } else if (reaction.emoji.id === acceptEmoji && count >= 3) {
+        } else if (reaction.emoji.id === rejectEmoji && count >= 3) {
             const embed = new Discord.RichEmbed({
                 title: 'Предложение отклонено:',
                 description: idea,
