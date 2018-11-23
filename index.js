96c6f097f10c24bd36b77ff8d70a17008db9da76
Sviridoff_E <Sviridoff_E@outlook.com>
2018-11-23 10:09:21 +0300

    Remove index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
-
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
