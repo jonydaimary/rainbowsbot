d2dfe3bd1ac3cf6cbabf37fbfb5104734070f2c5
Sviridoff_E <Sviridoff_E@outlook.com>
2018-11-24 22:24:48 +0300

    Update parse.js

---
+++ src/utils/parse.js
@@ -0,0 +1,27 @@
+const USER_MENTION_PATTERN = /^<@!?([0-9]+)>$/;
+const ROLE_MENTION_PATTERN = /^<@&!?([0-9]+)>$/;
+const ID_PATTERN = /^[0-9]+$/;
+
+module.exports.member = (guild, raw) => {
+    if (USER_MENTION_PATTERN.test(raw))
+        return guild.member(raw.match(USER_MENTION_PATTERN)[1]);
+    if (ID_PATTERN.test(raw))
+        return guild.member(raw);
+    return guild.members.find(member => member.displayName == raw || member.user.username == raw);
+};
+
+module.exports.user = (client, raw) => {
+    if (USER_MENTION_PATTERN.test(raw))
+        return client.fetchUser(raw.match(USER_MENTION_PATTERN)[1]);
+    if (ID_PATTERN.test(raw))
+        return client.fetchUser(raw);
+    return client.users.find(user => user.username == raw);
+};
+
+module.exports.role = (guild, raw) => {
+    if (ROLE_MENTION_PATTERN.test(raw))
+        return guild.roles.get(raw.match(ROLE_MENTION_PATTERN)[1]);
+    if (ID_PATTERN.test(raw))
+        return guild.roles.get(raw);
+    return guild.roles.find(role => role.name == raw);
+};
