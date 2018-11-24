3e9396abdd26e5fc8e3dc4e007c5c89898d3766c
Sviridoff_E <Sviridoff_E@outlook.com>
2018-11-24 22:25:22 +0300

    Add Util::role command

---
+++ src/commands/util/role.js
@@ -0,0 +1,34 @@
+const { RichEmbed } = require('discord.js');
+
+const Command = require('../../command-processing/command');
+const parse = require('../../utils/parse');
+
+const config = require('./../../../config');
+
+module.exports = new Command.Builder('role', 'Util')
+    .guildOnly()
+    .format('<роль>')
+    .description('Отображает информацию о роли')  
+    .validate((message, [role]) => {
+        if (!parse.role(message.guild, role))
+            return 'Роль не найдена';
+        return true;
+    })
+    .run((message, [role]) => {
+        role = parse.role(message.guild, role);
+        const embed = new RichEmbed()
+            .setTitle('Информация о роли')
+            .setColor(config.embed.color.guild)
+            .addField('Имя', role.name, true)
+            .addField('ID', role.id, true)
+            .addField('Создана', role.createdAt, true)
+            .addField('Участники', role.members.array().length, true)
+            .addField('Позиция', `${role.calculatedPosition}/${message.guild.roles.array().length}`, true)
+            .addField('Цвет', role.hexColor, true)
+            .addField('Отдельное отображение', role.hoist ? 'Да' : 'Нет', true)
+            .addField('Управляемая', role.managed ? 'Да' : 'Нет', true)
+            .addField('Упоминаемая', role.mentionable ? 'Да' : 'Нет', true)
+            .setFooter(message.author.tag, message.author.avatarURL);
+        message.channel.send(embed);
+    })
+    .build();
