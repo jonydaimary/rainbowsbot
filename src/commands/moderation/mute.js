907a01af98a3526e68805551a7cc13525c3d4452
Sviridoff_E <Sviridoff_E@outlook.com>
2018-11-30 21:35:18 +0300

    Add tempmute

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -1,3 +1,6 @@
+const { RichEmbed } = require('discord.js');
+const ms = require('ms');
+
 const Command = require('./../../command-processing/command');
 const parse = require('./../../utils/parse');
 
@@ -8,17 +11,39 @@ module.exports = new Command.Builder('mute', 'Moderation')
     .format('<пользователь>')
     .description('Замутить пользователя')
     .hasPermissions(member => member.hasPermission('MANAGE_MESSAGES'))
-    .validate((message, [member]) => {
+    .validate((message, [member, time]) => {
         member = parse.member(message.guild, member);
         if (!member)
             return 'Пользователь не найден';
         if (member.roles.has(config.roles.muted))
             return 'Пользователь уже замучен';
+        if (time) {
+            time = ms(time);
+            if (!time || typeof time == 'string' || time < 1)
+                return 'Неверный формат времени';
+        }
         return true;
     })
-    .run((message, [member]) => {
+    .run((message, [member, time]) => {
         member = parse.member(message.guild, member);
+        time = time ? ms(time) : time;
         member.addRole(config.roles.muted);
-        return `Пользователь ${member.user.tag} успешно замучен.`;
+        if (time) {
+            setTimeout(() => {
+                if (member.roles.has(config.roles.muted)) {
+                    member.removeRole(config.roles.muted);
+                    message.guild.channels.get(config.channels.staffchat)
+                        .send(`Пользователь ${member.user.tag} автоматически размучен по истечении ${ms(time, { long: true })}.`);
+                }
+            }, time);
+        }
+        const embed = new RichEmbed()
+            .setTitle('Пользователь замучен')
+            .setColor(config.embed.color.guild)
+            .addField('Модератор', message.author)
+            .addField('Нарушитель', member);
+        if (time)
+            embed.addField('Время', ms(time, { long: true }));
+        message.channel.send(embed);
     })
     .build();
