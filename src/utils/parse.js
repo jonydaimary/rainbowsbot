52bd96cf85c51ca52c9b6675328fc5caba5e9070
Sviridoff_E <Sviridoff_E@outlook.com>
2018-11-30 21:33:04 +0300

    Add parse.channel function

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,5 +1,6 @@
 const USER_MENTION_PATTERN = /^<@!?([0-9]+)>$/;
 const ROLE_MENTION_PATTERN = /^<@&!?([0-9]+)>$/;
+const CHANNEL_MENTION_PATTERN = /^<#!?([0-9]+)>$/;
 const ID_PATTERN = /^[0-9]+$/;
 
 module.exports.member = (guild, raw) => {
@@ -24,4 +25,12 @@ module.exports.role = (guild, raw) => {
     if (ID_PATTERN.test(raw))
         return guild.roles.get(raw);
     return guild.roles.find(role => role.name == raw);
+};
+
+module.exports.channel = (client, raw) => {
+    if (CHANNEL_MENTION_PATTERN.test(raw))
+        return client.channels.get(raw.match(CHANNEL_MENTION_PATTERN)[1]);
+    if (ID_PATTERN.test(raw))
+        return client.channels.get(raw);
+    return client.channels.find(channel => channel.name == raw);
 };
