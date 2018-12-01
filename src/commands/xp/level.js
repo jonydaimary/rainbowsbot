c601a6db2639c9919377dddb6186348e1ce45db2
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-01 23:02:59 +0300

    Add level command

---
+++ src/commands/xp/level.js
@@ -0,0 +1,27 @@
+const Command = require('../../command-processing/command');
+const parse = require('./../../utils/parse');
+
+module.exports = class LevelCommand extends Command {
+    constructor() {
+        super({
+            name: 'level',
+            group: 'Experience',
+            format: '[пользователь]',
+        });
+    }
+
+    validate(message, [user]) {
+        user = user ? parse.user(this.client, user) : message.author;
+        if (!user)
+            return 'Пользователь не найден';
+    }
+
+    async run(message, [user]) {
+        user = user ? parse.user(this.client, user) : message.author;
+        const data = await this.client.sequelize.model('users').xp(user.id);
+        return (user.id == message.author.id
+            ? 'Ваш текущий уровень: '
+            : `Текущий уровень пользователя ${user}: `)
+            + `**${data.level}** *${data.xp}/${data.nextLevelXp}*`;
+    }
+};
