f2e4a351befd5d9cb47c4006b15dfc0b72d2cfb1
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-02 00:11:32 +0300

    Add description for level command

--- src/commands/xp/level.js
+++ src/commands/xp/level.js
@@ -7,6 +7,7 @@ module.exports = class LevelCommand extends Command {
             name: 'level',
             group: 'Experience',
             format: '[пользователь]',
+            description: 'Отображает уровень пользователя.'
         });
     }
 
