eee575fa08cc65755f596069b123867dbd4b9fdd
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-01 23:34:54 +0300

    Update level command

--- src/commands/xp/level.js
+++ src/commands/xp/level.js
@@ -18,11 +18,16 @@ module.exports = class LevelCommand extends Command {
 
     async run(message, [user]) {
         user = user ? parse.user(this.client, user) : message.author;
+<<<<<<< HEAD
+        const data = await (this.client.sequelize.model('users')).xp(user.id);
+=======
         const data = await this.client.sequelize.model('users').xp(user.id);
-        await message.reply((user.id == message.author.id
+>>>>>>> 8e0d88d34ae38ac0f5114aab8b9558a5bc52375b
+        const response = (user.id == message.author.id
             ? 'Ваш текущий уровень: '
             : `Текущий уровень пользователя ${user}: `)
-            + `**${data.level}** *${data.xp}/${data.nextLevelXp}*`
-        );
+            + `**${data.level}** *${data.xp}/${data.nextLevelXp}*`;
+        console.log(response);
+        return response;
     }
 };
