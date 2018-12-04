e9ca62b41f0bb137b1ff4b7e045520af33d2b3bd
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-04 21:01:12 +0300

    Update say command

--- src/commands/util/say.js
+++ src/commands/util/say.js
@@ -1,8 +1,14 @@
 const Command = require('./../../command-processing/command');
 
+const PERMITTED_ROLES = ['Модератор', 'Главный Модератор', 'Администратор', 'Главный Администратор'];
+
 module.exports = new Command.Builder('say', 'Util')
     .format('[сообщение]')
     .description('Отправляет сообщение от имени бота')
     .parseArgs(rest => rest)
-    .run((message, content) => content ? message.channel.send(content) : undefined)
+    .hasPermissions(member => member.roles.some(role => PERMITTED_ROLES.includes(role.name)))
+    .run(async (message, content) => {
+        await message.delete();
+        return content ? message.channel.send(content) : undefined;
+    })
     .build();
