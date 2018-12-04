d34b8cafba97e21a5dc5f095811574c390295340
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-04 21:04:30 +0300

    Fix say command

--- src/commands/util/say.js
+++ src/commands/util/say.js
@@ -3,6 +3,7 @@ const Command = require('./../../command-processing/command');
 const PERMITTED_ROLES = ['Модератор', 'Главный Модератор', 'Администратор', 'Главный Администратор'];
 
 module.exports = new Command.Builder('say', 'Util')
+    .guildOnly()
     .format('[сообщение]')
     .description('Отправляет сообщение от имени бота')
     .parseArgs(rest => rest)
