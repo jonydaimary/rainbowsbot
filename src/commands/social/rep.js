b949cb9c1d828464ad1046ad6715ab18599a877d
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-07 17:01:15 +0300

    Add rep command

---
+++ src/commands/social/rep.js
@@ -0,0 +1,40 @@
+const Command = require('../../processing/commands/command');
+const parse = require('../../utils/parse');
+
+const PERMITTED_ROLES = ['Главный Администратор', 'Администратор', 'Главный Модератор', 'Модератор'];
+
+module.exports = class RepCommand extends Command {
+    constructor() {
+        super({
+            name: 'profile',
+            group: 'Social',
+            format: '[пользователь] [значение]',
+            description: 'Изменяет репутацию пользователя',
+            guildOnly: true
+        });
+    }
+
+    hasPermissions(member) {
+        return member.roles.some(role => PERMITTED_ROLES.includes(role.name));
+    }
+
+    validate(message, [user, value]) {
+        user = parse.user(this.client, user);
+        if (!user)
+            return 'Пользователь не найден';
+        if (!value)
+            return 'Значение репутации не указано';
+        if (isNaN(parseInt(value)))
+            return 'Неверный формат числа';
+        return true;
+    }
+
+    async run(message, [user, value]) {
+        user = parse.user(this.client, user);
+        value = parseInt(value);
+
+        const rep = await this.client.sequelize.model('users').rep(user.id, value, true);
+
+        return `Репутация пользователя ${user.tag} изменена: **${rep}**`;
+    }
+};
