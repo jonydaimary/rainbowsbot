18ea1d98b30759f51cb84db5b4d7f2b4794aa09f
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-07 17:01:07 +0300

    Update top command

--- src/commands/social/top.js
+++ src/commands/social/top.js
@@ -1,4 +1,4 @@
-const { stripIndent } = require('common-tags');
+const { stripIndent, stripIndents } = require('common-tags');
 
 const MessageFormatter = require('./../../utils/message-formatter');
 const nextLevelXp = require('./../../utils/next-level-xp');
@@ -10,31 +10,46 @@ module.exports = class TopCommand extends Command {
         super({
             name: 'top',
             group: 'Social',
-            format: '[кол-во пользователей от 1 до 10]',
-            description: 'Отображает топ пользователей по уровням'
+            format: '[rep] [кол-во пользователей от 1 до 10]',
+            description: 'Отображает топ пользователей по уровням или репутации',
+            details: stripIndents`
+                \`top [кол-во пользователей от 1 до 10]\`
+                Отображает топ пользователей по уровням.
+                \`top rep [кол-во пользователей от 1 до 10]\`
+                Отображает топ пользователей по репутации.
+            `
         });
     }
 
-    validate(message, [count]) {
+    validate(message, [rep, count]) {
+        rep = rep == 'rep';
+        if (!rep)
+            count = rep;
         count = count ? parseInt(count) : 10;
         if (isNaN(count))
             return 'Неверный формат чила';
         if (count < 1 || count > 10)
             return 'Кол-во пользователей должно быть числом между 1 и 10';
-        return true;
+        return [rep, count];
     }
 
-    async run(message, [count]) {
+    async run(message, [rep, count]) {
         count = count ? parseInt(count) : 10;
         const Users = this.client.sequelize.model('users');
-        const result = await Users.findAll({ limit: count, order: [['level', 'desc'], ['xp', 'desc']] });
+        const result = await Users.findAll({ limit: count, order: rep
+            ? [['reputation', 'desc']]
+            : [['level', 'desc'], ['xp', 'desc']] 
+        });
         const response = new MessageFormatter();
         result.forEach((r, i) => {
+            const scoreLine = rep
+                ? `\t\tРепутация: ${r.reputation}`
+                : `\t\tУровень: ${r.level} [${r.xp}/${nextLevelXp(r.level)}]`;
             response.line(stripIndent`
                 [${i + 1}]   ${i == 9 ? '' : ' '}> #${this.client.users.get(r.id).tag}
-                \t\tУровень: ${r.level} [${r.xp}/${nextLevelXp(r.level)}]
+                ${scoreLine}
             `);
         });
-        message.reply(`Топ ${count} пользователей по уровням:${response.toCode('pl')}`);
+        message.reply(`Топ ${count} пользователей по ${rep ? 'репутации' : 'уровням'}:${response.toCode('pl')}`);
     }
 };
