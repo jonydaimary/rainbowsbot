22656df2dc0ca93d5881e881d825a8b5fb6fb55c
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-14 23:47:58 +0300

    Update warn function

--- src/functions/warn.js
+++ src/functions/warn.js
@@ -1,9 +1,9 @@
-module.exports = async (client, member, moderator, reason) => {
+module.exports = async (client, member, moderator, reason, timestamp) => {
     const Warns = client.sequelize.model('warns');
     const warn = Warns.build({
         member: member.user.id,
         moderator: moderator.user.id,
-        reason
+        reason, timestamp
     });
     await warn.save();
     client.emit('memberWarned', warn);
