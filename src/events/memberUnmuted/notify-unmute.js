d0337becd33fa84add637fb4c58b2d997d98c00a
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-14 23:47:32 +0300

    Add unmute notifications

---
+++ src/events/memberUnmuted/notify-unmute.js
@@ -0,0 +1,8 @@
+const ms = require('ms');
+
+const { channels: { staffchat } } = require('../../../json/config.json');
+
+module.exports = (client, member, time) => {
+    client.guild().channels.get(staffchat)
+        .send(`Пользователь ${member.user.tag} был размучен${time ? ` по истечении ${ms(time)}.` : '.'}`);
+};
