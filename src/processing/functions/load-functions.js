781219c81fad3d55880994b4c4565c29f0c45c54
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-14 23:11:12 +0300

    Update function loading

--- src/processing/functions/load-functions.js
+++ src/processing/functions/load-functions.js
@@ -3,5 +3,5 @@ const requireAll = require('require-all');
 module.exports = (object, dirname) => {
     const dir = requireAll({ dirname });
     for (const fn in dir)
-        object.prototype[fn] = dir[fn].bind(undefined, object);
+        object[fn] = dir[fn].bind(undefined, object);
 };
