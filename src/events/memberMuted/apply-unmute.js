462f3c7958ddb11f158f1b84a9872fea87eff395
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-15 00:22:29 +0300

    Update apply-unmute.js

--- src/events/memberMuted/apply-unmute.js
+++ src/events/memberMuted/apply-unmute.js
@@ -4,6 +4,6 @@ module.exports = async (client, member, time) => {
     if (!time)
         return;
     const TempMutes = client.sequelize.model('tempmutes');
-    const tempMute = await TempMutes.findById(member.user.id);
+    const tempMute = await TempMutes.findByPk(member.user.id);
     unmuteCallback(member, tempMute.timestamp, time);
 };
