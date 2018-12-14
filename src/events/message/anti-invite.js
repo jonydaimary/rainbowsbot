589a2a1cfee3b0ce8e37e2c44130bee074533aa0
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-14 23:05:13 +0300

    Add anti invite protection

---
+++ src/events/message/anti-invite.js
@@ -0,0 +1,12 @@
+const INVITE_LINK_PATTERN = /(?:https?:\/\/)?(?:www\.)?(?:discord\.(?:gg|io|me|li)|discordapp\.com\/invite)\/(.+[a-z])/g;
+
+module.exports = (client, message) => {
+    if (message.channel.type != 'text')
+        return;
+    if (!INVITE_LINK_PATTERN.test(INVITE_LINK_PATTERN))
+        return;
+    message.delete();
+    const moderator = client.guild().member(client.user.id);
+    client.warn(message.member, moderator, 'Рассылка приглашений');
+    message.reply('Вы получили предупреждение за рассылку приглашений.');
+};
