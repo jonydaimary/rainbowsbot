2e3eac15a38b0597eacc82a34528874d140b4995
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-15 21:53:08 +0300

    Fix notify mute

--- src/events/memberMuted/notify-mute.js
+++ src/events/memberMuted/notify-mute.js
@@ -4,5 +4,5 @@ const { channels: { staffchat } } = require('../../../json/config.json');
 
 module.exports = async (client, member, time) => {
     client.guild().channels.get(staffchat)
-        .send(`Пользователь ${member.tag} замучен${time ? ` на ${ms(time, { long: true })}.` : '.'}`);
+        .send(`Пользователь ${member.user.tag} замучен${time ? ` на ${ms(time, { long: true })}.` : '.'}`);
 };
