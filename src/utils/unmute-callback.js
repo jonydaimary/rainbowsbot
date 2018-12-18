2a80584d89e66b9d5381b6b60df59815233c9d03
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-18 22:52:28 +0300

    Fix unmuting unmuted member

--- src/utils/unmute-callback.js
+++ src/utils/unmute-callback.js
@@ -2,6 +2,8 @@ const { roles: { muted } } = require('../../json/config.json');
 
 module.exports = (member, timestamp, time) => {
     const unmute = async () => {
+        if (!member.roles.has(muted))
+            return;
         await member.removeRole(muted);
         await member.client.sequelize.model('tempmutes').remove(member.user.id);
         member.client.emit('memberUnmuted', member, time);
