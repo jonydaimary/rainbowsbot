dd4642853ed84a31e3a51b17161c2f9647c47c0d
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-23 20:29:35 +0300

    .

--- src/rest.js
+++ src/rest.js
@@ -6,17 +6,17 @@ const PORT = process.env.PORT || 8080;
 const api = express();
 api.use(bodyParser.json());
 
-module.exports = client => {
+require('./dbinit').then(sequelize => {
     api.get('/user:id', async (req, res) => {
-        const user = await client.sequelize.model('users').findByPk(req.param('id'));
+        const user = await sequelize.model('users').findByPk(req.param('id'));
         res.json(user
             ? user
             : { error: 'Invalid user ID.' }
         );
     });
-
+    
     api.get('/warns:id', async (req, res) => {
-        const warns = await client.sequelize.model('warns').get(req.param('id'));
+        const warns = await sequelize.model('warns').get(req.param('id'));
         res.json(warns
             ? warns
             : { error: 'Invalid user ID.' }
@@ -24,4 +24,4 @@ module.exports = client => {
     });
 
     api.listen(PORT, () => console.log(`API listening on port: ${PORT}`));
-};
+});
