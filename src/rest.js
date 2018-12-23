df835d00f069259a35c178cf30bd16aa7917d6eb
Sviridoff_E <Sviridoff_E@outlook.com>
2018-12-23 21:55:54 +0300

    Update rest

--- src/rest.js
+++ src/rest.js
@@ -7,29 +7,22 @@ const api = express();
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded());
 
-let sequelize;
+require('./dbinit')().then(sequelize => {
+    api.get('/api/user:id', async (req, res) => {
+        const user = await sequelize.model('users').findByPk(req.param('id'));
+        res.json(user
+            ? user
+            : { error: 'Invalid user ID.' }
+        );
+    });
+    
+    api.get('/api/warns:id', async (req, res) => {
+        const warns = await sequelize.model('warns').get(req.param('id'));
+        res.json(warns
+            ? warns
+            : { error: 'Invalid user ID.' }
+        );
+    });
 
-require('./dbinit')().then(_sequelize => {
-    sequelize = _sequelize;
     api.listen(port, () => console.log(`API listening on port: ${port}`));
-});
-
-api.get('/', async (req, res) => {
-    res.send('Rainbow`s system');
-});
-
-api.get('api/user:id', async (req, res) => {
-    const user = await sequelize.model('users').findByPk(req.param('id'));
-    res.json(user
-        ? user
-        : { error: 'Invalid user ID.' }
-    );
-});
-
-api.get('api/warns:id', async (req, res) => {
-    const warns = await sequelize.model('warns').get(req.param('id'));
-    res.json(warns
-        ? warns
-        : { error: 'Invalid user ID.' }
-    );
 });
